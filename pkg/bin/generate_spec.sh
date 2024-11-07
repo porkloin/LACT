bfded8fdf29fb4c947de1e439daed642b82a260f
porkloin <rybateman@gmail.com>
2024-11-06 23:47:35 -0900

    better substring match

--- pkg/bin/generate_spec.sh
+++ pkg/bin/generate_spec.sh
@@ -27,8 +27,8 @@ for RECIPE_PATH in "$RECIPES_DIR"/*/; do
   SOURCE_URL="https://github.com/ilya-zlobintsev/LACT/archive/refs/tags/v${RECIPE_VERSION}.tar.gz"
 
   # Collect Fedora-specific dependencies safely
-  PKG_DEPENDS=$(yq eval 'select(.depends != null) | (.depends | with_entries(select(.key | test("fedora")))) | .[] // [] | join(" ")' "$RECIPE_FILE" | xargs)
-  PKG_BUILD_DEPENDS=$(yq eval 'select(.build_depends != null) | (.build_depends | with_entries(select(.key | test("fedora")))) | .[] // [] | join(" ")' "$RECIPE_FILE" | xargs)
+  PKG_DEPENDS=$(yq eval '.depends | with_entries(select(.key | contains("fedora"))) | .[] | join(" ")' "$RECIPE_FILE" | xargs)
+  PKG_BUILD_DEPENDS=$(yq eval '.build_depends | with_entries(select(.key | contains("fedora"))) | .[] | join(" ")' "$RECIPE_FILE" | xargs)
 
   # Include dependencies from the 'all' key if they exist
   ALL_DEPENDS=$(yq eval 'select(.depends.all != null) | .depends.all | join(" ")' "$RECIPE_FILE" | xargs)
@@ -46,7 +46,7 @@ for RECIPE_PATH in "$RECIPES_DIR"/*/; do
   cat <<EOF >"$SPEC_FILE"
 Name:           $RECIPE_NAME
 Version:        $RECIPE_VERSION
-Release:        $RECIPE_RELEASE%{?dist}
+Release:        $RECIPE_RELEASE
 Summary:        $PKG_DESCRIPTION
 License:        $PKG_LICENSE
 URL:            https://github.com/ilya-zlobintsev/LACT
